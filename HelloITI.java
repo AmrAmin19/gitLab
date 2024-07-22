afd55ca030dd8a4935ccd7969183eb2dd12b801d
AmrAmin19 <100246329+AmrAmin19@users.noreply.github.com>
2024-07-22 15:53:55 +0300

    firstCommit

---
+++ HelloITI.java
@@ -0,0 +1,13 @@
+public class HelloITI {
+    public static void main(String[] args) {
+        printHelloITI();
+    }
+	
+    public static void printHelloGit() {
+        System.out.println("Hello git");
+    }
+
+    public static void printHelloITI() {
+        System.out.println("Hello, ITI!");
+    }
+}
